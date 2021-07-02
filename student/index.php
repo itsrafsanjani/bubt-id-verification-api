3d6cc0d1c197d04a1e706178f9c24a7e7cdde87b
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-07-02 17:45:34 +0600

    form design updated

--- student/index.php
+++ student/index.php
@@ -49,18 +49,37 @@ if (isset($_POST['btn'])) {
 
 ?>
 
-<link rel="stylesheet" href="./css/bootstrap.css">
-<link rel="stylesheet" href="./css/signin.css">
-<link rel="stylesheet" href="./css/style.css">
+<!DOCTYPE html>
+<html lang="en">
 
-<form action="" method="POST" class="form-signin">
-    <div class="container">
-        <div class="text-center">
-            <img src="img/studlab-animated.svg" alt="" width="250" height="250">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faculty Verification</title>
+    <link rel="stylesheet" href="./css/bootstrap.css">
+</head>
+
+<body>
+    <div class="container vh-100">
+        <div class="row vh-100 justify-content-center align-items-center">
+            <div class="col-md-4 mx-auto">
+                <div class="card shadow">
+                    <div class="card-body">
+                        <form method="POST">
+                            <div class="text-center">
+                                <img src="img/studlab-animated.svg" alt="studlab-logo" class="img-fluid my-5">
+                            </div>
+                            <label for="studentId">Enter Your ID: </label>
+                            <input type="text" name="student_id" id="studentId" class="form-control mb-3" placeholder="Enter full ID here..">
+                            <p> <?php echo $message; ?> </p>
+                            <input type="submit" name="btn" class="btn btn-lg btn-primary btn-block" value="Verify">
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <label for="studentId">Enter Your ID: </label>
-        <input type="text" name="student_id" id="studentId" class="form-control mb-3" placeholder="Enter full ID here..">
-        <p> <?php echo $message; ?> </p>
-        <input type="submit" name="btn" class="btn btn-lg btn-primary btn-block" value="Verify">
     </div>
-</form>
+</body>
+
+</html>
